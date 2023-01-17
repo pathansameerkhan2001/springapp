fd53c39cca5989a78de354a9935b9a2d417b497f
pathansameerkhan2001.Sameer <pathansameerkhan2001.sameerk0601@gmail.com>
2023-01-17 17:29:02 +0530

    spring commit

---
+++ spring-applocation/src/main/java/com/example/springapplocation/SpringApplocationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springapplocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApplocationApplication.class, args);
+	}
+
+}
---
+++ spring-applocation/src/test/java/com/example/springapplocation/SpringApplocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springapplocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApplocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
